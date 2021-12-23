c191d89d61d6e3383ce64fe3c18572a2523b7892
Ajwme55 <ajwme@iname.com>
2021-12-23 11:02:43 +0300

    Added gamemode input

--- Hangman/Hangman.py
+++ Hangman/Hangman.py
@@ -3,3 +3,4 @@ wordList_def = ("Computer", "Flame", "Mathematics", "Condition", "Frigid")  # A 
 wordList_cust = [""]  # An initialized word list that will contain user input words (for custom mode)
 
 print("Would you like to use a default set of words or run in custom mode with words of your own ?")
+gameMode = input("Enter D to play in Default mode or C to play in Custom mode")
