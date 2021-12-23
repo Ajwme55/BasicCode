77f2221bc5a11e5d4a7309d964aae6a2fc0cce47
Ajwme55 <ajwme@iname.com>
2021-12-23 10:52:18 +0300

    Added info

---
+++ Hangman.py
@@ -0,0 +1,3 @@
+print("Welcome to Hangman !")
+wordList_def = ("Computer", "Flame", "Mathematics", "Condition", "Frigid")  # A word tuple with some default words (for default mode)
+wordList_cust = [""]  # An initialized word list that will contain user input words (for custom mode)
