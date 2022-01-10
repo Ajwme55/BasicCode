023e2ea6e48956f2c627d4bb4e41a86cc6e905b2
Ajwme55 <ajwme@iname.com>
2022-01-10 10:27:14 +0300

    Completed, regular and custom modes implemented with lives system - some more input checks can be added but no issues are found

--- Hangman/Conduct_Game.py
+++ Hangman/Conduct_Game.py
@@ -1,7 +1,52 @@
 def Game(wordList):
-    print("GAME OCCURS HERE")
-    Win = 1
+    import random
+    gameWord = wordList[random.randrange(0,len(wordList))]  # Gives the gameword, chosen randomly from list given
 
+    lenWord = len(gameWord)  # An often used quantity saved as a variable for readability 
+    gameWord_letterStatus = [0] * lenWord  # A list that holds if each letter of the word has been discovered (by default nothing is discovered and is all 0s)
+    runStatus = 1  # 1 if the game is the game running, 0 if it over
+    lives = 10  # Editable value, number of lives given to player before "hanging"
+
+    while runStatus == 1 :
+        print("")  # Some spacing
+
+        print("Try to guess the word :",end =' ')  # End set to blank so as to not go to a new line
+        for x in range(0,lenWord) :  # Loop to display whole word
+            if gameWord_letterStatus[x] == 0 :
+                print("_",end =' ')
+            elif gameWord_letterStatus[x] == 1 :
+                print(gameWord[x],end =' ')
+            else :
+                print("Error detected (CODE 3)")  # This should not happen
+                Result = 3
+                break
+
+        print(".Now guess a letter that belongs :",end =' ')  # NEED TO ADD CHECK TO MAKE SURE INPUT IS ONE LETTER + CHECK TO AVOID DUPLICATE INPUTS
+        guess = input()
+        badguess = 1  # If a match is found anywhere in the word the guess will no longer be bad
+        for x in range(0,lenWord) :  # Loop to check through whole word if guess letter is found
+            #print(gameWord[x],end =' ')  # THIS IS A DEBUGGING TOOL, KEEP IT COMMENTED OUT - displays the gameWord letter by letter
+            if gameWord[x] == guess :  # match found, guess is good
+                gameWord_letterStatus[x] = 1
+                badguess = 0  
+        if badguess == 0 :
+            print("Good job, this letter was found")
+        else : # badguess has to be 1 here
+            lives = lives-1  # Player loses 1 life due to wrong guess
+            print("Sorry, that letter was not found, you have",lives,"lives remaining")
+
+        if lives == 0 :
+            print("Out of lives ! The man is hanged !")
+            Result = 0
+            runStatus = 0  # Not actually needed due to break
+            break
+
+        if 0 in gameWord_letterStatus :  # Any letters still not discovered
+            print("There are more letters to find !") 
+        else :  # All values in gameWord_letterStatus have been set to 1 (cannot be anything else) and thus word is found
+            print("Wow you found all the letters for the word",gameWord,"- the man is saved !")
+            Result = 1
+            runStatus = 0
     
 
-    return Win
+    return Result
--- Hangman/Hangman.py
+++ Hangman/Hangman.py
@@ -1,6 +1,6 @@
 import Conduct_Game
 
-print("Welcome to Hangman !")
+print("Welcome to Hangman ! Please note that this game is case sensitive")
 wordList_def = ("Computer", "Flame", "Mathematics", "Condition", "Frigid")  # A word tuple with some default words (for default mode)
 
 print("Would you like to use a default set of words or run in custom mode with words of your own ?")
@@ -8,19 +8,27 @@ gameMode = 0
 gameResult = -1  # Will state if game was won or lost
 
 while gameMode == 0 :
-    gameMode = input("Enter 1 to play in Default Mode or 2 to play in Custom Mode : ")  # APPEARS TO NOT UPDATE gameMode ????
+    gameMode = int(input("Enter 1 to play in Default Mode or 2 to play in Custom Mode : "))  # Typecasts to int as input() gives a string
 
     if gameMode == 1 : 
         print("Launching game with default list of", len(wordList_def), "words")
         gameResult = Conduct_Game.Game(wordList_def)
     elif gameMode == 2 :
-        print("Enter upto 10 words you want to use - hit enter after each word ! ")
+        latestWord = []  # THIS MAY NEED IMPROVMENT
+        print("Enter upto 10 words you want to use - hit enter after each word and type DONE when finished")
         newWord = input()
+        if newWord == 'DONE' :
+            print("Thus there are no words at all - exiting now")
+            gameResult = 2  # Quit by user input
+            break
         wordList_cust = [newWord]  # An initialized word list that will contain user input words (for custom mode)
-        while len(wordList_cust<10) :
+        while len(wordList_cust)<10 :
             newWord = input()
-            wordList_cust = wordList_cust + newWord
-        print("Launching game with a custom word list of ", len(wordList_cust), "words")
+            if newWord == 'DONE' :
+                break
+            latestWord = [newWord]  # THIS (SEE ABOVE) MAY NEED IMPROVMENT - IS DONE TO GET THE LATEST INPUT AS A LIST INSTEAD OF A STRING
+            wordList_cust = wordList_cust + latestWord
+        print("Launching game with a custom word list of", len(wordList_cust), "words")
         gameResult = Conduct_Game.Game(wordList_cust)
     else :  # error if user puts gameMode not as 1 or 2
         print("Error,",gameMode,"is not a valid Game Mode")
@@ -32,6 +40,8 @@ if gameResult == 1 :
     print("You win !")
 elif gameResult == 0 :
     print("You Lose !")
+elif gameResult == 2 :
+    print("You Quit !") 
 else :
-    print("Error detected in game result")  # This should not trigger
+    print("Error detected !")  # This should not trigger
  
